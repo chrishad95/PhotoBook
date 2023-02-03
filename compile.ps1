0a46633a8f868acb7569b612e5943d76199610fd
chrishad95 <chrishadley@gmail.com>
2023-02-03 11:40:35 -0500

    update the compile script
    update the compile script with some checks for files existing and make a backup of previous compiles

--- compile.ps1
+++ compile.ps1
@@ -1,4 +1,32 @@
-$sourceFile = "ImageMontage.cs"
-$outputFile = "ImageMontage.exe"
 
-& csc.exe /out:$outputFile $sourceFile
+
+$sourceFile = "ImageMontage.cs"
+$exeFile = "ImageMontage.exe"
+
+# check if the source file exists
+if (!(Test-Path $sourceFile)) {
+    Write-Output "Source file not found: $sourceFile"
+    exit
+}
+
+# check if the executable file already exists
+if (Test-Path $exeFile) {
+    # create a backup of the executable file
+    $timestamp = Get-Date -Format yyyy-MM-dd_HH-mm-ss
+    $backupFile = $exeFile + "_" + $timestamp + ".bak"
+    Move-Item $exeFile $backupFile
+    Write-Output "Backed up $exeFile to $backupFile"
+}
+
+# compile the source file
+$compiler = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe"
+& $compiler /out:$exeFile $sourceFile
+
+# check if the executable file was created
+if (!(Test-Path $exeFile)) {
+    Write-Output "Failed to create executable file: $exeFile"
+    exit
+}
+
+# output success message
+Write-Output "Successfully compiled $sourceFile into $exeFile"
